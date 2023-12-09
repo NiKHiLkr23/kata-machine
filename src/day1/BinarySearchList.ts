a8dec5aa644e6fd150e9d36a63968551b40eea46
NiKHiLkr23 <nikhilkumarofficial23@gmail.com>
2023-12-09 15:49:04 +0530

    refactor: Binarysearch update

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,16 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+    let left = 0;
+    let right = haystack.length;
 
+    while (left < right) {
+        let mid = Math.floor(left + (right - left) / 2);
+        if (haystack[mid] === needle) {
+            return true;
+        } else if (haystack[mid] < needle) {
+            left = mid + 1;
+        } else {
+            right = mid;
+        }
+    }
+    return false;
 }
